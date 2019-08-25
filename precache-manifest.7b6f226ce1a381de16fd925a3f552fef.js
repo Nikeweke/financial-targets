7bc3507a70ac1287641d1d783ab8dbd410989901
Nikeweke <jxammet@gmail.com>
2019-08-25 09:24:56 +0300

    init

---
+++ precache-manifest.7b6f226ce1a381de16fd925a3f552fef.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/fonts/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/fonts/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c0152733cb8329f37820",
+    "url": "/js/app.0e82dc4c.js"
+  },
+  {
+    "revision": "7eab9517fb943be34309",
+    "url": "/js/chunk-vendors.ab02adaa.js"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/fonts/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "d69914fc875e7447475b",
+    "url": "/js/about.0047eef0.js"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/fonts/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/img/flags.9c74e172.png"
+  },
+  {
+    "revision": "d61a9204a64bed88e3a764281fe189e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/img/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/img/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "c0152733cb8329f37820",
+    "url": "/css/app.4227f533.css"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/fonts/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/img/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/fonts/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/fonts/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/fonts/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/fonts/icons.faff9214.woff"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/fonts/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/fonts/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/fonts/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "7eab9517fb943be34309",
+    "url": "/css/chunk-vendors.95574c8f.css"
+  }
+];
